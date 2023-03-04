45360453e2a594ed5fb0e75d9fffa02ac8a8344d
SteGarAla <stegarc@ut.utm.edu>
2023-03-03 20:20:58 -0600

    Add project files.

---
+++ CW9Threading/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace CW9Threading
+{
+
+    //the first of two classes needed
+    public class FindPiThread
+    {
+        //attributes
+
+        //Number of darts that will be thrown
+        int amountOfDartsNeedToThrow;
+        //Number of darts that have landed
+        int DartsLanded;
+        //will be used to generate throws
+        Random random;
+
+        public FindPiThread(int Darts2Throw)
+        {
+            int rando = random.
+        }
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //the second of the two programs needed
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
